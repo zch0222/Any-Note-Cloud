46326a8aeb17d52e466c172906823d487b3a6af4
称霸幼儿园 <yxlmzch@163.com>
2024-06-24 01:02:05 +0800

    update: whisper

--- anynote-modules/anynote-modules-ai-nio/src/main/java/com/anynote/ai/nio/service/impl/WhisperServiceImpl.java
+++ anynote-modules/anynote-modules-ai-nio/src/main/java/com/anynote/ai/nio/service/impl/WhisperServiceImpl.java
@@ -2,6 +2,7 @@ package com.anynote.ai.nio.service.impl;
 
 import com.anynote.ai.nio.model.dto.WhisperDTO;
 import com.anynote.ai.nio.model.vo.WhisperSubmitVO;
+import com.anynote.ai.nio.model.vo.WhisperVO;
 import com.anynote.ai.nio.service.WhisperService;
 import com.anynote.common.redis.constant.RedisChannel;
 import com.anynote.common.redis.service.ConfigService;
@@ -39,35 +40,49 @@ public class WhisperServiceImpl implements WhisperService {
 
     @Override
     public Flux<ServerSentEvent<String>> whisper(WhisperDTO whisperDTO) {
-        Flux<ServerSentEvent<String>> heartbeatFlux = Flux.interval(Duration.ofSeconds(10))
-                .map(tick -> {
-                    log.info("heartbeat");
-                    return ServerSentEvent.<String>builder()
-                            .id(new Date().toString())
-                            .data("heartbeat")
-                            .event("heartbeat")
-                            .build();
-                });
+//        Flux<ServerSentEvent<String>> heartbeatFlux = Flux.interval(Duration.ofSeconds(10))
+//                .map(tick -> {
+//                    log.info("heartbeat");
+//                    return ServerSentEvent.<String>builder()
+//                            .id(new Date().toString())
+//                            .data("heartbeat")
+//                            .event("heartbeat")
+//                            .build();
+//                });
 
         String aiServiceAddress = configService.getAIServerAddress();
+//        return webClient.post()
+//                .uri(aiServiceAddress + "/api/whisper/submit")
+//                .body(Mono.just(whisperDTO), WhisperDTO.class)
+//                .retrieve()
+//                .bodyToMono(WhisperSubmitVO.class)
+//                .flux()
+//                .flatMap(whisperSubmitVO ->
+//                    Flux.merge(heartbeatFlux, reactiveRedisTemplate
+//                        .listenToChannel(RedisChannel.WHISPER_TASK_CHANNEL + whisperSubmitVO.getTaskId())
+//                        .map(value -> {
+//                            log.info(value.toString());
+//                            return ServerSentEvent.<String>builder()
+//                                    .id(String.valueOf(System.currentTimeMillis()))
+//                                    .data(gson.toJson(value.getMessage()))
+//                                    .event("message")
+//                                    .build();
+//                        }))
+//                );
+//        return Flux.merge(heartbeatFlux);
+
         return webClient.post()
-                .uri(aiServiceAddress + "/api/whisper/submit")
+                .uri(aiServiceAddress + "/api/whisper")
                 .body(Mono.just(whisperDTO), WhisperDTO.class)
                 .retrieve()
-                .bodyToMono(WhisperSubmitVO.class)
-                .flux()
-                .flatMap(whisperSubmitVO ->
-                    Flux.merge(heartbeatFlux, reactiveRedisTemplate
-                        .listenToChannel(RedisChannel.WHISPER_TASK_CHANNEL + whisperSubmitVO.getTaskId())
-                        .map(value -> {
-                            log.info(value.toString());
-                            return ServerSentEvent.<String>builder()
-                                    .id(String.valueOf(System.currentTimeMillis()))
-                                    .data(gson.toJson(value.getMessage()))
-                                    .event("message")
-                                    .build();
-                        }))
-                );
-//        return Flux.merge(heartbeatFlux);
+                .bodyToFlux(WhisperVO.class)
+                .flatMap(whisperVO -> {
+                    log.info(gson.toJson(whisperVO));
+                    return Flux.just(ServerSentEvent
+                            .<String>builder()
+                            .id(String.valueOf(System.currentTimeMillis()))
+                            .event("message")
+                            .data(gson.toJson(whisperVO)).build());
+                });
     }
 }
