8bae50a47aed2d842351ffee79f166eb3340d1be
称霸幼儿园 <yxlmzch@163.com>
2024-07-30 09:21:20 +0800

    update: AIFastApiChatService chatCompletions

--- anynote-common/anynote-common-ai-fastapi/src/main/java/com/anynote/ai/fastapi/core/AIFastApiChatService.java
+++ anynote-common/anynote-common-ai-fastapi/src/main/java/com/anynote/ai/fastapi/core/AIFastApiChatService.java
@@ -3,6 +3,7 @@ package com.anynote.ai.fastapi.core;
 import com.anynote.ai.fastapi.model.dto.FastApiChatCompletionsDTO;
 import com.anynote.ai.fastapi.model.vo.FastApiChatCompletionsVO;
 import com.anynote.ai.fastapi.properties.AIFastApiProperties;
+import com.anynote.common.redis.service.ConfigService;
 import com.anynote.core.utils.StringUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
@@ -19,13 +20,15 @@ public class AIFastApiChatService {
     @Resource
     private WebClient webClient;
 
+//    @Resource
+//    private AIFastApiProperties aiFastApiProperties;
     @Resource
-    private AIFastApiProperties aiFastApiProperties;
+    private ConfigService configService;
 
 
     public Flux<FastApiChatCompletionsVO> chatCompletions(FastApiChatCompletionsDTO chatCompletionsDTO) {
         return webClient.post()
-                .uri(StringUtils.format("{}/v1/chat/completions", aiFastApiProperties.getAddress()))
+                .uri(StringUtils.format("{}/v1/chat/completions", configService.getAIServerAddress()))
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(chatCompletionsDTO))
                 .retrieve()
